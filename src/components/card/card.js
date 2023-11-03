1946dd6fafd99b509a0c9113ec93008c72fa8970
Maksim noyt <maksimpv1@bk.ru>
2023-11-03 10:30:16 +0300

    introduce project from private rep

---
+++ src/components/card/card.js
@@ -0,0 +1,239 @@
+import {init} from '../../modules/shoppingCart/cart.js';
+
+const cardRoot = document.getElementById("card-root")
+function getCards(pagesCards){
+    const url = new URL(`https://64675bd9ba7110b663b6500e.mockapi.io/products?page=${pagesCards}&limit=10`);
+    fetch(url) 
+    .then((response) => {
+        if(response.status === 200) {
+            return response.json();
+        }else{
+            throw Error(response);
+        }
+    })
+    .then((json) =>{  
+        console.log(json)
+        const btnNext = document.querySelector('.pageSwapnext')
+        if(json.length === 0){
+            pagesCards = pagesCards - 1
+            numberOfPage(pagesCards)
+            btnNext.classList.add('pageSwapnext--hide')
+            btnNext.removeEventListener("click",counterNext);
+        }else{
+            btnNext.classList.remove('pageSwapnext--hide')
+            numberOfPage(pagesCards) 
+            createCards(json)
+            init()
+        }
+    })
+    .catch((error) => {
+        alert(error)
+    })
+
+}
+
+function createCards(elements){
+    const list = document.createElement("div");
+    list.classList.add("products");
+    for(let product of Object.entries(elements)){
+        var element = product[1]
+        createCard(element,list)
+    }
+    cardRoot.prepend(list)
+}
+
+function createCard(product,list){
+    const deletePages = document.querySelector('.products');
+    if(deletePages){
+        deletePages.remove()
+    }
+
+    const item = document.createElement("div");
+    item.classList.add("product");
+    const viewButton = createButtonElement("btnhide", "Быстрый просмотр");
+    const buyButton = createButtonElement("btnBuy", "Buy");
+    item.innerHTML = `
+    <div class="card-wrapper">
+        <p class="card-name">${product.name}</p>
+        <div class="card-view">
+            <img src=${product.images} alt=${product.name} />
+        </div>
+        <div class="card-info">            
+            <p class="product-price">$${product.price}</p>
+        </div>
+    </div>
+    `;
+    item.setAttribute('data-id',`${product.id}`)
+    list.insertBefore(item,list.children[1])
+    const buyDivBTN = item.querySelector(".card-info")
+    buyDivBTN.appendChild(buyButton)
+    const viewDivBTN = item.querySelector(".card-view")
+    viewDivBTN.appendChild(viewButton)
+    item.setAttribute("name",product.name)
+
+    item.addEventListener("mouseenter", () => showinfo(viewButton));
+    item.addEventListener("mouseleave", () => hideinfo(viewButton));
+    viewButton.addEventListener("click",() => modalView(product,list))
+
+
+   
+}
+function createButtonElement (className, children) {
+    const button = document.createElement("button")
+    className && button.classList.add(className)
+    button.setAttribute("type","button")
+    button.setAttribute("id","btn-view-card")
+    if(typeof children === 'string'){
+        button.innerHTML = children
+    }else{
+        button.append(children)
+    }
+    
+    return button
+}
+
+function showinfo(event){
+    event.classList.remove("btnhide")
+    event.classList.add("btnshow")
+    
+}
+function hideinfo(event){
+    event.classList.remove("btnshow")
+    event.classList.add("btnhide")
+}
+
+function modalView(product,list){
+    const item = document.createElement("div");
+    item.classList.add("chil");
+    const buyButton = createButtonElement("btnBuy", "Buy");
+    item.innerHTML = `
+    <div class="modal">
+        <div class="modal_overlay">
+            <div class="modal_card">
+                <div class="modal_close">
+                    <div class="modal_close-wrapper">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <h2 class="modal_card-name">${product.name}</h2>
+                <div class="modal_info">
+                    <div class="modal_img">
+                        <img src="${product.images}">
+                    </div>                    
+                    <div class="modal_main-info">
+                        <div class="modal_description">
+                            <h5>Описание: </h5>
+                            <p>${product.description}</p>
+                        </div>
+                        <div class="modal_addinfo">
+                            <div class="modal_price">
+                                <h5>Цена: </h5>
+                            <p>${product.price} $</p>
+                            </div>
+                            <div class="modal_price"> 
+                                <h5>В наличии:</h5> 
+                                <p>${product.countInStock} штук</p>
+                            </div>        
+                            <div class="modal_brand"> 
+                                <h5>Брэнд: </h5>
+                                <p>${product.brand}</p> 
+                            </div>
+                            <div class="modal_brand">
+                                <h5>Рейтинг:</h5> 
+                                <p>${product.rating} баллов</p>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+
+    list.lastChild.className != "chil" ? list.append(item) : console.log("Элемент уже создан") 
+    const modals = document.querySelector('.chil');
+    const modalOverlay = document.querySelector('.modal_overlay');
+    const modalCard = document.querySelector('.modal_card');
+
+    const buyDivBTN = item.querySelector(".modal_card")
+    buyDivBTN.appendChild(buyButton)
+
+    modalVisible(modalOverlay,modalCard,modals)
+    
+}
+function modalVisible(value1,value2,value3){
+    value1.style.opacity = '1';
+    value1.style.visibility = 'visible';
+    value2.style.display = 'block';
+    const modalWindowClose = value3.parentNode
+
+    const btnClose = document.querySelector('.modal_close-wrapper')
+    btnClose.addEventListener('click',(event)=> {
+        modalWindowClose.removeChild(value3)
+    })
+
+    value1.addEventListener('click',(event)=> {
+        if(event.target == value1){
+            modalWindowClose.removeChild(value3)
+        }
+    })
+}
+
+function nextPage(){
+    const pageSwap = document.createElement('div')
+    pageSwap.classList.add('pageSwap')
+    cardRoot.append(pageSwap)
+
+    const previousBtnSpan = document.createElement('span')
+    const nextBtnSpan = document.createElement('span')
+
+    const previousPageCard = createButtonElement("pageSwapprevious", previousBtnSpan);
+    pageSwap.append(previousPageCard)
+    //previousPageCard.append(document.createElement('span'))
+
+    const nextCard = createButtonElement("pageSwapnext", nextBtnSpan);
+    pageSwap.append(nextCard)
+   // nextCard.append(document.createElement('span'))
+
+    nextCard.addEventListener("click",counterNext)
+
+
+    previousPageCard.addEventListener("click", () =>counterNext(1))
+}
+let pagesCards = 1
+function counterNext(value){   
+        if(typeof value === "object"){
+            pagesCards = pagesCards + 1;
+            getCards(pagesCards)
+        }else{
+            pagesCards > 1 ? pagesCards = pagesCards - 1 : alert("Вы на первой странице")
+            console.log(pagesCards)
+            getCards(pagesCards)
+            const nextCard = document.querySelector('.pageSwapnext')
+            nextCard.addEventListener("click",counterNext)
+        }
+}
+
+function numberOfPage(numberPage){
+    const previousNumberPageDelete = document.querySelector('.pageNumber')
+    
+    if(previousNumberPageDelete){
+        previousNumberPageDelete.remove()
+    }
+    const numberPageDiv = document.createElement('div')
+    numberPageDiv.classList.add('pageNumber')
+
+    cardRoot.prepend(numberPageDiv)
+
+    numberPageDiv.innerHTML = `
+    <p>Страница ${numberPage}</p>    
+    `
+
+}
+
+document.addEventListener("DOMContentLoaded", getCards(1));
+nextPage()
+
+
---
+++ src/modules/shoppingCart/cart.js
@@ -0,0 +1,213 @@
+const openPopUpBtn = document.getElementById('open-modal-btn');
+const popUp = document.getElementById('cart-modal');
+const closePopUpBtn = document.getElementById('close-modal-btn');
+
+// открыть модальное окно
+openPopUpBtn.addEventListener('click', openPopUp)
+function openPopUp(e){
+    e.preventDefault();
+    popUp.classList.add('open')
+}
+
+// закрыть модальное окно
+closePopUpBtn.addEventListener('click', function(){
+    popUp.classList.remove('open')
+})
+
+
+// закрыть модальное окно при нажатии на esc
+window.addEventListener('keydown', (e) =>{
+    if(e.key === 'Escape'){
+        popUp.classList.remove('open')
+    }
+});
+
+// закрыть модальное окно при клике вне него
+document.querySelector('#cart-modal .cart-modal__box').addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+popUp.addEventListener('click', event => {
+    if(event._isClickWithInModal) return
+    event.currentTarget.classList.remove('open');
+})
+
+
+//функции корзины
+const cartList = document.querySelector('.cart-modal__list');
+const cart = document.querySelector('.cart-modal');
+const cartCounter = document.querySelector('.cart__counter');
+const deleteAllBtn = document.querySelector('.btn-clear-all');
+const fullPrice = document.querySelector('.cart-modal__full-price');
+let price = 0;
+
+
+// //переводить число в строку удаляя пробелы и знаки валюты
+const priceWithoutSpaces = (str) => {
+	return str.replace(/\D/g,'');
+};
+
+//отрисовка корзины
+const renderCart = (img, title, price, id) =>{
+    return `
+    <li class="cart-modal__item" data-id="${id}">
+        <div class="cart-modal__item-img">
+            <img src="${img}" alt=""></div>
+        <div class="cart-modal__text">
+            <div class="cart-modal__item-descr">
+                <h3>${title}</h3>
+            </div>
+            <div class="cart-modal__item-counters">
+                <button class="cart-btn-plus" data-id="${id}">+</button>
+                <input class="cart-counter-input" type=""text" value="1" disabled>
+                <button class="cart-btn-minus" data-id="${id}">-</button>
+            </div>
+            <div class="cart-modal__item-price">
+                <p >${price} $</p>
+                <button class="cart-btn-delete">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" d="M15.4624 7.625H18.5249V9.375H17.1365L16.37 17.8063C16.3085 18.4824 15.7417 19 15.0629 19H8.86193C8.1831 19 7.61628 18.4824 7.55482 17.8063L6.78834 9.375H5.3999V7.625H8.4624V6.3125C8.4624 5.58763 9.05003 5 9.7749 5H14.1499C14.8748 5 15.4624 5.58763 15.4624 6.3125V7.625ZM10.2124 6.75V7.625H13.7124V6.75H10.2124ZM8.54556 9.375L9.26147 17.25H14.6633L15.3792 9.375H8.54556Z" fill="currentColor"></path></svg>
+                </button>
+            </div>
+        </div>
+    </li>`;
+}
+
+
+//нажимая на card-кнопку передаем нужные для добавления в корзину и ее отрисовки
+function init(){
+    const productsBtn = document.querySelectorAll('.btnBuy');
+
+    productsBtn.forEach(el => {
+        el.addEventListener('click', (e) => {
+            let self = e.currentTarget;
+            let parent = self.closest('.product');
+            let id = parent.dataset.id;
+            console.log(id)
+            let img = parent.querySelector('img').getAttribute('src');
+            let title = parent.querySelector('.card-name').textContent;
+            let priceNumber = +priceWithoutSpaces(parent.querySelector('.product-price').textContent);
+    
+            plusFullPrice(priceNumber);
+            printFullPrice();
+            //копипуем данные с card в cart
+            cartList.insertAdjacentHTML('afterbegin', renderCart(img, title, priceNumber, id));
+            printCounter();
+    
+            // addStoreElement(id)
+            updateStorage();
+    
+            self.disabled = true;
+        });
+    });
+    
+}
+
+//setTimeout(init, 2000);
+
+//суммировать общую цену
+function plusFullPrice(currentPrice){
+    return price +=currentPrice;
+}
+
+//вычитать из общей суммы
+function minusFullPrice(currentPrice){
+    return price -=currentPrice;
+}
+
+//функция выводящая общ сумму на страницу
+function printFullPrice(){
+    fullPrice.textContent = `Итого: ${price} $`
+}
+
+// счетчик сверху у кнопки корзины
+function printCounter(){
+    let productsListLength = cartList.children.length;
+    cartCounter.textContent = productsListLength;
+}
+
+//удаление
+cartList.addEventListener('click', (e) => {
+    let deleteBtn = e.target.parentNode;
+    if(deleteBtn.classList.contains('cart-btn-delete')){
+        deleteProduct(deleteBtn.closest('.cart-modal__item'));
+    }
+});
+
+function deleteProduct(productParent) {
+
+    let id = productParent.dataset.id;
+    // console.log(id)
+    if(document.querySelector(`.product[data-id="${id}"]`)){
+        document.querySelector(`.product[data-id="${id}"]`).querySelector('.btnBuy').disabled = false;
+    }
+
+    let currentPrice = +priceWithoutSpaces(productParent.querySelector('.cart-modal__item-price').textContent);
+	minusFullPrice(currentPrice);
+    printFullPrice();
+	productParent.remove();
+
+	printCounter();
+
+    updateStorage();
+}
+
+
+//localStorage
+function initState(){
+    if(localStorage.getItem('products') !== null){
+        // console.log(localStorage.getItem('products'));
+        cartList.innerHTML = localStorage.getItem('products');
+        printCounter();
+        countSum();
+        printFullPrice();
+
+        // оставляем кнопки товаров из корзины неактивными после обновления
+        document.querySelectorAll('.cart-modal__item').forEach(el => {
+            let id = el.dataset.id;
+            console.log(id);
+            // document.querySelector(`.product[data-id="${id}"]`).querySelector('.btnBuy').disabled = true;
+        });
+    }
+}
+initState()
+
+//добавить или удалить из localStorage
+function updateStorage(){
+    let parent = cartList;
+    let html = parent.innerHTML;
+    html = html.trim();  // чтобы удалить пробелы
+    // console.log(html);
+    // console.log(html.length);
+    if(html.length){
+        localStorage.setItem('products', html);
+    }else{
+        localStorage.removeItem('products');
+    }
+}
+
+//считает сумму после localStorage
+function countSum() {
+    document.querySelectorAll('.cart-modal__item-price').forEach(el => {
+        price += +priceWithoutSpaces(el.textContent);
+    });
+};
+
+
+// удалить всё
+deleteAllBtn.addEventListener('click', ()=> {
+    while (cartList.firstChild) {
+        cartList.removeChild(cartList.firstChild);
+    }
+    localStorage.clear()
+    console.log('hi');
+    fullPrice.textContent = `Итого: 0 $`
+    printCounter();
+    const allBtns = document.querySelectorAll('.btnBuy')
+    allBtns.forEach((productBtn) => {
+        productBtn.removeAttribute('disabled')
+    })
+
+    // document.querySelector('.btnBuy').disabled = false;
+});
+
+
+export {init};
